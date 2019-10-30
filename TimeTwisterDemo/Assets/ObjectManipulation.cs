f0eb72b3cb878e270c14c07e47504f86696e6578
Thimo <tj.smits@outlook.com>
2019-10-30 22:53:19 +0100

    added jump and basic object interaction

---
+++ TimeTwisterDemo/Assets/ObjectManipulation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectManipulation : MonoBehaviour
+{
+
+    public float InteractionDistance = 3f;
+
+    public Image crosshair;
+
+    private Interactable interactable;
+
+    private new Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = FindObjectOfType<Camera>();
+        crosshair.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        // Center of screen
+        Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out hit, InteractionDistance))
+        {
+            if (hit.transform.CompareTag("Interactable"))
+            {
+                crosshair.enabled = true;
+
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    interactable = hit.collider.GetComponent<Interactable>();
+                    interactable.SetGravity(false);
+                    Vector3 position = new Vector3(Screen.width / 2, Screen.height / 2, 2f);
+                    interactable.MoveToTarget(camera.ScreenToWorldPoint(position));
+                }
+            }
+        } else
+        {
+            if (crosshair.enabled)
+            {
+                interactable.SetGravity(true);
+                crosshair.enabled = false;
+            }
+        }
+    }
+}
---
+++ TimeTwisterDemo/Assets/Scripts/Mechanics/PlayerMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float jumpHeight = 3.0f;
+
+    public float speed = 12f;
+
+    public float gravity = -9.81f;
+
+    private Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = IsCharacterGrounded();
+
+        ResetGravityWhenNearGround();
+
+        Move();
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+
+        ApplyGravity();
+    }
+
+    private bool IsCharacterGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+    }
+
+    private void Move()
+    {
+        // Obtain the axes inputs
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float zInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = transform.right * xInput + transform.forward * zInput;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+    }
+
+    private void ResetGravityWhenNearGround()
+    {
+        if (isGrounded && velocity.y < 0.0f)
+        {
+            velocity.y = -2.0f;
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
